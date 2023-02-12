49785c3c83fa115812af310fd28f03632fa7a592
JonMS95 <jon_portugalete@hotmail.com>
2023-02-12 17:30:02 +0100

    Refactored the code. Split it all into functions.

--- Source_files/main.c
+++ Source_files/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <sys/socket.h> // socket, bind, listen accept functions.
-#include <stdlib.h>     // EXIT_FAILURE
-#include <arpa/inet.h>  // sockaddr_in, inet_addr
-#include <unistd.h>     // Write socket.
-#include <string.h>     // strcpy
-#include <netinet/tcp.h>
+#include <sys/socket.h>     // socket, bind, listen accept functions.
+#include <stdlib.h>         // EXIT_FAILURE
+#include <arpa/inet.h>      // sockaddr_in, inet_addr
+#include <unistd.h>         // Write socket.
+#include <string.h>         // strcpy
+#include <netinet/tcp.h>    // SO_KEEPALIVE
 
 #define TEST_PORT       8080    // TCP testing port.
 #define CONN_NUM        3       // Maximum number of concurrent connections that the socket may attend to.
@@ -12,17 +12,29 @@
 #define GREETING_SIZE   100
 #define RX_BUFFER_SIZE  256     // RX buffer size.
 
-/*
-@brief Main function. Program's entry point.
-*/
-int main(int argc, char** argv)
+typedef enum
 {
-    // Create socket descriptor.
-    // Use AF_INET if the socket is meant to be serving to another computer in the same net.
-    // Use AF_LOCAL the purpose is to communicate different processes within the same host.
-    // Use SOCK_STREAM for TCP, SOCK_DGRAM for UDP.
-    // Use 0 to use the internet protocol (IP).
-    int socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    DESCRIPTOR = 0  ,
+    OPTIONS         ,
+    PARAMETERS      ,
+    BIND            ,
+    LISTEN          ,
+    ACCEPT          ,
+    WRITE           ,
+    READ            ,
+    CLOSE           ,
+
+}SOCKET_STATE;
+
+/// @brief Create socket descriptor.
+/// @param domain Use AF_INET if the socket is meant to be serving to another computer in the same net,
+/// AF_LOCAL if the purpose is to communicate different processes within the same host.
+/// @param type Use SOCK_STREAM for TCP, SOCK_DGRAM for UDP.
+/// @param protocol Use IPPROTO_IP (0) to use the internet protocol (IP).
+/// @return  a file descriptor > 0 number if the socket descriptor was succesfully created, -1 if any error happened. 
+int CreateSocketDescriptor(int domain, int type, int protocol)
+{
+    int socket_desc = socket(domain, type, protocol);
 
     if(socket_desc < 0)
     {
@@ -31,12 +43,19 @@ int main(int argc, char** argv)
     }
     printf("Socket file descriptor created.\r\n");
 
-    // Set socket options.
-    int reuse_address   = 1;
-    int reuse_port      = 1;
-    int keep_idle       = 1;
-    int keep_counter    = 5;
-    int keep_interval   = 5;
+    return socket_desc;
+}
+
+/// @brief Set socket options.
+/// @param socket_desc Previously created socket descriptor.
+/// @param reuse_address 1 if the address is meant to be forcefully used again, 0 otherwise.
+/// @param reuse_port 1 if the port is meant to be forcefully used again, 0 otherwise.
+/// @param keep_idle defines heartbeat frequency when it's receiving packets ACK packets from the other side (server is continuouly sending empty packets).
+/// @param keep_counter dictates how many unanswered heartbeats will indicate a broken connection.
+/// @param keep_interval defines heartbeat frequency when there is no answer from the client's side.
+/// @return < 0 if any error happened.
+int SocketOptions(int socket_desc, int reuse_address, int reuse_port, int keep_idle, int keep_counter, int keep_interval)
+{
     int socket_options;
 
     socket_options = setsockopt(socket_desc, SOL_SOCKET, SO_REUSEADDR , &reuse_address, sizeof(reuse_address    ));
@@ -51,6 +70,16 @@ int main(int argc, char** argv)
     }
     printf("Successfully set socket options.\r\n");
 
+    return socket_options;
+}
+
+/// @brief Prepare the sockaddr_in struct for the binding process to be completed properly.
+/// @param address_family Address family the server is going to work with.
+/// @param allowed_IPs Use INADDR_ANY to allow any IP, specify it otherwise.
+/// @param listen_port The port the server is going to be later listening to.
+/// @return A struct which contains all the parameters related to a socket descriptor. 
+struct sockaddr_in PrepareForBinding(sa_family_t address_family, in_addr_t allowed_IPs, uint16_t listen_port)
+{
     // Prepare the sockaddr_in structure for the binding process.
     struct sockaddr_in server;
     server.sin_family = AF_INET;            // IPv4.
@@ -58,21 +87,53 @@ int main(int argc, char** argv)
     server.sin_port = htons(TEST_PORT);     // The htons() function makes sure that numbers are stored in memory
                                             // in network byte order, which is with the most significant byte first.
 
+    return server;
+}
+
+/// @brief Binds socket to previously specified address and port.
+/// @param socket_desc Socket descriptor.
+/// @param server Previously defined server parameters struct.
+/// @return < 0 if the binding failed.
+int BindSocket(int socket_desc, struct sockaddr_in server)
+{
     // Bind: attach the socket descriptor to a particular IP and port.
     socklen_t file_desc_len = (socklen_t)sizeof(struct sockaddr_in);
-    int bind_socket_desc = bind(socket_desc, (struct sockaddr*)&server, file_desc_len);
-    if(bind_socket_desc  < 0)
+    int bind_socket = bind(socket_desc, (struct sockaddr*)&server, file_desc_len);
+    if(bind_socket  < 0)
     {
         perror("Socket binding failed");
         exit(EXIT_FAILURE);
     }
     printf("Socket file descriptor binded.\r\n");
 
-    // Listen for incoming connections.
-    listen(socket_desc, CONN_NUM);
+    return bind_socket;
+}
 
+/// @brief Listen for incoming connections.
+/// @param socket_desc Socket descriptor.
+/// @param connections_number Maximum number of allowed connections.
+/// @return < 0 if listening failed.
+int SocketListen(int socket_desc, int connections_number)
+{
+    int socket_listen = listen(socket_desc, CONN_NUM);
+
+    if(socket_listen < 0)
+    {
+        perror("Socket listen failed");
+    }
+    printf("Socket listen succeed.\r\n");
+
+    return socket_listen;
+}
+
+/// @brief Accept an incoming connection.
+/// @param socket_desc Previously created socket descriptor.
+/// @return New socket instance, based on the socket descriptor.
+int SocketAccept(int socket_desc)
+{
     // Wait for incoming connections.
     struct sockaddr_in client;
+    socklen_t file_desc_len = (socklen_t)sizeof(struct sockaddr_in);
     int new_socket = accept(socket_desc, (struct sockaddr*)&client, (socklen_t*)&file_desc_len);
 
     if(new_socket < 0)
@@ -82,7 +143,7 @@ int main(int argc, char** argv)
     printf("Connection accepted.\r\n");
 
     int keep_alive = 1;
-    socket_options = setsockopt(new_socket, SOL_SOCKET, SO_KEEPALIVE , &keep_alive, sizeof(keep_alive));
+    int socket_options = setsockopt(new_socket, SOL_SOCKET, SO_KEEPALIVE , &keep_alive, sizeof(keep_alive));
 
     // Send a message to the client as soon as it is accepted.
     char client_ip_addr[IP_ADDR_SIZE + 1];
@@ -95,6 +156,15 @@ int main(int argc, char** argv)
     
     write(new_socket, greeting, sizeof(greeting));
 
+    return new_socket;
+}
+
+
+/// @brief Reads from client. No perror statement exists within this function's definition, as read function can return something <= 0 if client gets disconnected.
+/// @param new_socket Socket instance, based on the previously defined socket descriptor. 
+/// @return <= 0 if read failed. The state where something > 0 is returned should never be reached by now.
+int SocketRead(int new_socket)
+{
     char rx_buffer[RX_BUFFER_SIZE];
     memset(rx_buffer, 0, sizeof(rx_buffer));
 
@@ -110,13 +180,48 @@ int main(int argc, char** argv)
         }
         else if(read_from_socket <= 0)
         {
-            printf("An error happened while reading.\r\n");
+            printf("An error happened while reading (client disconnected).\r\n");
             break;
         }
     }
 
+    return read_from_socket;
+}
+
+int CloseSocket(int new_socket)
+{
     // Close the socket.
-    close(new_socket);
+    int close_socket = close(new_socket);
+
+    if(close_socket < 0)
+    {
+        perror("An error happened while closing the socket");
+    }
+    printf("Socket successfully closed.\r\n");
+
+    return close_socket;
+}
+
+/*
+@brief Main function. Program's entry point.
+*/
+int main(int argc, char** argv)
+{
+    int socket_desc = CreateSocketDescriptor(AF_INET, SOCK_STREAM, IPPROTO_IP);
+
+    int socket_options = SocketOptions(socket_desc, 1, 1, 1, 5, 5); 
+
+    struct sockaddr_in server = PrepareForBinding(AF_INET, INADDR_ANY, TEST_PORT);
+
+    int bind_socket = BindSocket(socket_desc, server);
+
+    int listen = SocketListen(socket_desc, CONN_NUM);
+
+    int new_socket = SocketAccept(socket_desc);
+
+    int read = SocketRead(new_socket);
+
+    int close = CloseSocket(new_socket);
 
     return 0;
 }
