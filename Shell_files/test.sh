ad07451b3cdd6beef7ed9d58e6bd1e478a4bdd6b
JonMS95 <jon_portugalete@hotmail.com>
2023-02-17 16:47:10 +0100

    Modified test.sh file, for it to be suitable for passing command line input parameters.

--- Shell_files/test.sh
+++ Shell_files/test.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+DEFAULT_USER_PORT=50000
+DEFAULT_CONN_NUM=1
+
 echo
 echo "*******************************"
 echo "Testing 'main' executable file."
 echo "*******************************"
-./Executable_files/main
+./Executable_files/main ${DEFAULT_USER_PORT} ${DEFAULT_CONN_NUM}
