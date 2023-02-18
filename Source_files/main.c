5c38b49aad80ce85c2864e3bfe8ab7310a81ac22
JonMS95 <jon_portugalete@hotmail.com>
2023-02-18 18:28:32 +0100

    Added a mechanism for the options to be parsed automatically. WIP.

--- Source_files/main.c
+++ Source_files/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h> // memset, strlen
 #include "socket_use.h" // IF_ANET, SOCK_STREAM, ...
+#include <getopt.h> // getopt, optarg, ...
 
 #define ARGV_PORT_IDX           1
 #define ARGV_MAX_CONN_NUM_IDX   2
@@ -51,6 +52,10 @@ option_description opt_desc[2] =
     }
 };
 
+/// @brief Generates a string including the options that are meant to be inputed properly formatted.
+/// @param option_descr Array of option_description structs. Each one includes information about a parameter.
+/// @param option_descr_size Size of the previous input parameter.
+/// @param opt_short Short form of command line arguments, properñy formatted.
 void PreParseOptions(option_description* option_descr, int option_descr_size, char* opt_short)
 {
     char option_short[option_descr_size * 2 + 1];
@@ -67,6 +72,47 @@ void PreParseOptions(option_description* option_descr, int option_descr_size, ch
     }
 
     strcpy(opt_short, option_short);
+}
+
+int ParseArguments(int argc, char** argv, option_description* option_descr, int option_descr_size, char* opt_short)
+{
+    int option;
+
+    while((option = getopt(argc, argv, opt_short)) != -1)
+    {
+        for(int i = 0; i < option_descr_size; i++)
+        {
+            if(option == option_descr[i].opt_char)
+            {
+                if(option_descr[i].assigned_value != 0)
+                {
+                    printf("Value already assigned to the current parameter.\r\n");
+                    return -1;
+                }
+                else
+                {
+                    if(option_descr[i].has_value == HAS_VALUE_ON)
+                    {
+                        if(optarg != NULL)
+                        {
+                            int option_int = atoi(optarg);
+                            if(option_int < option_descr[i].min_value || option_int > option_descr[i].max_value)
+                            {
+                                return -1;
+                            }
+
+                            option_descr[i].assigned_value = option_int;
+                        }
+                        else
+                        {
+                            printf("Provide a value for the input parameter [%s].\r\n", option_descr[i].detail);
+                            return -1;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 /// @brief Parse server's port.
@@ -129,6 +175,8 @@ int main(int argc, char** argv)
     int opt_desc_size = sizeof(opt_desc) / sizeof(opt_desc[0]);
     char options_short[opt_desc_size * 2 + 1];
     PreParseOptions(opt_desc, opt_desc_size, options_short);
+
+    int parse_arguments = ParseArguments(argc, argv, opt_desc, opt_desc_size, options_short);
 
     int server_port = ParsePort(argv);
     if(server_port < 0)
