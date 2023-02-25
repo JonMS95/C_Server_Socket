4e04efb358446b2250de0d8ad3c0429bb7de7f2f
JonMS95 <jon_portugalete@hotmail.com>
2023-02-25 11:35:36 +0100

    Added new directories (dependencies). By now, this are being fetched from another directory in the local PC. Looking forward to download those files directly from Github somehow.

--- Shell_files/directories.sh
+++ Shell_files/directories.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+if [ ! -d Object_files ]; then
+    mkdir Object_files
+fi
+
 if [ ! -d Executable_files ]; then
     mkdir Executable_files
+fi
+
+if [ ! -d Dependency_files ]; then
+    mkdir Dependency_files
+fi
+
+if [ ! -d Dependency_files/Header_files ]; then
+    mkdir Dependency_files/Header_files
+fi
+
+if [ ! -d Dependency_files/Dynamic_libraries ]; then
+    mkdir Dependency_files/Dynamic_libraries
 fi
