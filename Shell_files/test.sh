75bde340799b1bdebdf7250137d365033ab0406e
JonMS95 <jon_portugalete@hotmail.com>
2023-03-10 15:36:12 +0100

    Added export path sentence in test.sh file

--- Shell_files/test.sh
+++ Shell_files/test.sh
@@ -3,6 +3,8 @@
 DEFAULT_USER_PORT=50000
 DEFAULT_CONN_NUM=1
 
+export LD_LIBRARY_PATH=~/Desktop/scripts/C/C_Socket/Dependency_files/Dynamic_libraries
+
 echo
 echo "*******************************"
 echo "Testing 'main' executable file."
