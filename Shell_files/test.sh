a347487e9b21897b96b694e241fabd310b873c73
JonMS95 <jon_portugalete@hotmail.com>
2023-10-09 20:00:10 +0200

    Test file slighlty modified as new locations were provided for main test files as well as for dyn libs.

--- Shell_files/test.sh
+++ Shell_files/test.sh
@@ -3,10 +3,10 @@
 DEFAULT_USER_PORT=50000
 DEFAULT_CONN_NUM=1
 
-export LD_LIBRARY_PATH=~/Desktop/scripts/C/C_Socket/Dependency_files/Dynamic_libraries
+export LD_LIBRARY_PATH=~/Desktop/scripts/C/C_Socket/Tests/Dependency_files/Dynamic_libraries
 
 echo
 echo "*******************************"
 echo "Testing 'main' executable file."
 echo "*******************************"
-./Executable_files/main -p ${DEFAULT_USER_PORT} -c ${DEFAULT_CONN_NUM}
+./Tests/Executable_files/main -p ${DEFAULT_USER_PORT} -c ${DEFAULT_CONN_NUM}
