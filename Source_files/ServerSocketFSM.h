3c62c01f0a6357c6a5b25e2a030dff6e8ac08bb1
JonMS95 <jon_portugalete@hotmail.com>
2023-10-21 15:39:34 +0200

    Added message define statements. Remove Doxygen comments that used to be above function prototypes.

--- Source_files/ServerSocketFSM.h
+++ Source_files/ServerSocketFSM.h
@@ -1,7 +1,37 @@
 #ifndef SERVER_SOCKET_FSM_H
 #define SERVER_SOCKET_FSM_H
 
+/************************************/
+/******** Include statements ********/
+/************************************/
+
 #include "ServerSocket_api.h"
+
+/************************************/
+
+/***********************************/
+/******** Define statements ********/
+/***********************************/
+
+#define SERVER_SOCKET_MSG_CREATION_NOK      "Socket file descriptor creation failed."
+#define SERVER_SOCKET_MSG_CREATION_OK       "Socket file descriptor created."
+#define SERVER_SOCKET_MSG_SET_OPTIONS_NOK   "Failed to set socket options."
+#define SERVER_SOCKET_MSG_SET_OPTIONS_OK    "Successfully set socket options."
+#define SERVER_SOCKET_MSG_BIND_NOK          "Socket binding failed."
+#define SERVER_SOCKET_MSG_BIND_OK           "Socket file descriptor binded."
+#define SERVER_SOCKET_MSG_LISTEN_NOK        "Socket listen failed."
+#define SERVER_SOCKET_MSG_LISTEN_OK         "Socket listen succeeded."
+#define SERVER_SOCKET_MSG_ACCEPT_NOK        "Accept failed."
+#define SERVER_SOCKET_MSG_ACCEPT_OK         "Accept succeeded."
+#define SERVER_SOCKET_CLIENT_DISCONNECTED   "Client disconnected."
+#define SERVER_SOCKET_MSG_CLOSE_NOK         "An error happened while closing the socket."
+#define SERVER_SOCKET_MSG_CLOSE_OK          "Socket successfully closed."
+
+/************************************/
+
+/**********************************/
+/******** Type definitions ********/
+/**********************************/
 
 typedef enum
 {
@@ -15,40 +45,18 @@ typedef enum
 
 } SOCKET_FSM;
 
-/// @brief Create socket descriptor.
-/// @return < 0 if it failed to create the socket.
+/**********************************/
+
+/*************************************/
+/******** Function prototypes ********/
+/*************************************/
+
 int SocketStateCreate();
-
-/// @brief Set socket options.
-/// @param socket_desc Socket file descriptor.
-/// @return < 0 if it failed to set options.
 int SocketStateOptions(int socket_desc);
-
-/// @brief Bind socket to an IP address and port.
-/// @param socket_desc Socket file descriptor.
-/// @param server_port The port which is going to be used to listen to incoming connections.
-/// @return < 0 if it failed to bind.
 int SocketStateBind(int socket_desc, int server_port);
-
-/// @brief Listen to incoming connections.
-/// @param socket_desc Socket file descriptor.
-/// @param max_conn_num Maximum connections number.
-/// @return < 0 if it failed to listen.
 int SocketStateListen(int socket_desc, int max_conn_num);
-
-/// @brief Accept an incoming connection.
-/// @param socket_desc Socket file descriptor.
-/// @return < 0 if it failed to accept the connection.
 int SocketStateAccept(int socket_desc);
-
-/// @brief Read data received in the socket.
-/// @param new_socket Socket file descriptor.
-/// @return <= 0 if it failed to read.
 int SocketStateRead(int new_socket);
-
-/// @brief Close socket.
-/// @param new_socket Socket file descriptor.
-/// @return < 0 if it failed to close the socket.
 int SocketStateClose(int new_socket);
 
 #endif
