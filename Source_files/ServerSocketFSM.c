561770614724f698236fd903bd439589753e996a
JonMS95 <jon_portugalete@hotmail.com>
2023-10-27 19:23:11 +0200

    Modified FSM to manage concurrent client connections, as well as to create a server socket instance (if possible) for each of those clients.

--- Source_files/ServerSocketFSM.c
+++ Source_files/ServerSocketFSM.c
@@ -2,11 +2,12 @@
 /******** Include statements ********/
 /************************************/
 
+#include <stdlib.h>         // malloc, calloc, realloc, free.
 #include <unistd.h>         // Fork if concurrency is accepted.
-#include <sys/types.h>      // Find child processes.
-#include <sys/wait.h>       // Find child processes.
+#include <string.h>         // strcpy
 #include "ServerSocketUse.h"
 #include "ServerSocketFSM.h"
+#include "ServerSocketConcurrency.h"
 #include "SeverityLog_api.h"
 
 /************************************/
@@ -99,9 +100,9 @@ int SocketStateListen(int socket_desc, int max_conn_num)
 /// @return < 0 if it failed to accept the connection.
 int SocketStateAccept(int socket_desc)
 {
-    int new_socket = SocketAccept(socket_desc);
+    int client_socket = SocketAccept(socket_desc);
 
-    if(new_socket < 0)
+    if(client_socket < 0)
     {
         LOG_ERR(SERVER_SOCKET_MSG_ACCEPT_NOK);
     }
@@ -110,25 +111,82 @@ int SocketStateAccept(int socket_desc)
         LOG_INF(SERVER_SOCKET_MSG_ACCEPT_OK);
     }
     
-    return new_socket;
+    return client_socket;
+}
+
+/// @brief Manage concurrent server instances.
+/// @param client_socket Client socket descriptor.
+/// @param server_instance_processes Array which contains PIDs of each running server socket instance.
+/// @param max_conn_num Maximum amount of allowed clients (== server socket instances).
+/// @return < 0 if new instance could not be created. Otherwise, 0 if current process is parent, 1 if it is child server isntance.
+int SocketStateManageConcurrency(int client_socket, pid_t* server_instance_processes, int max_conn_num)
+{
+    // Check if any server socket instance may be created.
+    int possible_new_instance = ServerSocketPossibleNewInstance(server_instance_processes, max_conn_num);
+
+    if(possible_new_instance < 0)
+    {
+        LOG_WNG(SERVER_SOCKET_MSG_MAX_CONNS_REACHED);
+        return -1;
+    }
+
+    int retfork = fork();
+
+    if(retfork < 0)
+    {
+        LOG_ERR(SERVER_SOCKET_MSG_CANNOT_FORK);
+        return -2;
+    }
+
+    if(retfork > 0)
+    {
+        int new_server_instance_index = ServerSocketNewInstanceSpotIndex(server_instance_processes, max_conn_num);
+
+        if(new_server_instance_index < 0)
+        {
+            return -1;
+        }
+
+        server_instance_processes[new_server_instance_index] = retfork;
+
+        LOG_DBG(SERVER_SOCKET_MSG_NEW_PROCESS, retfork);
+
+        return 0;
+    }
+
+    return 1;
+}
+
+int SocketStateRefuse(int client_socket)
+{
+    char refusal_msg[SERVER_SOCKET_LEN_MSG_REFUSE] = {};
+    sprintf(refusal_msg, SERVER_SOCKET_MSG_REFUSE, SERVER_SOCKET_SECONDS_AFTER_REFUSAL);
+
+    write(client_socket, refusal_msg, strlen(refusal_msg));
+
+    sleep(SERVER_SOCKET_SECONDS_AFTER_REFUSAL);
+
+    int close_socket = SocketStateClose(client_socket);
+
+    return close_socket;
 }
 
 /// @brief Read data received in the socket.
-/// @param new_socket Socket file descriptor.
+/// @param client_socket Socket file descriptor.
 /// @return <= 0 if it failed to read.
-int SocketStateRead(int new_socket)
+int SocketStateRead(int client_socket)
 {
-    int read = SocketRead(new_socket);
+    int read = SocketRead(client_socket);
 
     return read;
 }
 
 /// @brief Close socket.
-/// @param new_socket Socket file descriptor.
+/// @param client_socket Socket file descriptor.
 /// @return < 0 if it failed to close the socket.
-int SocketStateClose(int new_socket)
+int SocketStateClose(int client_socket)
 {
-    int close = CloseSocket(new_socket);
+    int close = CloseSocket(client_socket);
     
     if(close < 0)
     {
@@ -150,8 +208,8 @@ int ServerSocketRun(int server_port, int max_conn_num, bool concurrent)
 {
     SOCKET_FSM socket_fsm = CREATE_FD;
     int socket_desc;
-    int new_socket;
-    int server_instances_count = 0;
+    int client_socket;
+    pid_t* server_instances = (pid_t*)calloc(max_conn_num, sizeof(pid_t));
 
     while(1)
     {
@@ -188,9 +246,9 @@ int ServerSocketRun(int server_port, int max_conn_num, bool concurrent)
 
             case ACCEPT:
             {
-                new_socket = SocketStateAccept(socket_desc);
-                
-                if(new_socket >= 0)
+                client_socket = SocketStateAccept(socket_desc);
+
+                if(client_socket >= 0)
                 {
                     if(!concurrent)
                     {
@@ -198,57 +256,65 @@ int ServerSocketRun(int server_port, int max_conn_num, bool concurrent)
                         continue;
                     }
 
-                    if(server_instances_count >= max_conn_num)
-                    {
-                        LOG_WNG(SERVER_SOCKET_MSG_MAX_CONNS_REACHED);
-
-                        int status;
-
-                        // TO DO. Use waitpid here. Keep in mind that the number returned by fork() function is the PID of the recently created process.
-                    }
-
-                    int retfork = fork();
-
-                    if(retfork < 0)
-                    {
-                        LOG_ERR(SERVER_SOCKET_MSG_CANNOT_FORK);
-                        continue;
-                    }
-
-                    if(retfork > 0)
-                    {
-                        LOG_INF("retfork = %d", retfork);
-                        server_instances_count++;
-                        LOG_INF(SERVER_SOCKET_MSG_SERVER_INSTANCES, server_instances_count);
-                        continue;
-                    }
-
-                    LOG_DBG(SERVER_SOCKET_MSG_NEW_PROCESS, getpid());
-                    socket_fsm = READ;
+                    socket_fsm = MANAGE_CONCURRENCY;
                 }
+            }
+            break;
+
+            case MANAGE_CONCURRENCY:
+            {
+                int manage_concurrency = SocketStateManageConcurrency(client_socket, server_instances, max_conn_num);
+
+                switch(manage_concurrency)
+                {
+                    case SERVER_SOCKET_ERR_REFUSE_CONN:
+                    {
+                        socket_fsm = REFUSE;
+                    }
+                    break;
+
+                    case SERVER_SOCKET_SUCCESS_PARENT:
+                    case SERVER_SOCKET_ERR_CANNOT_FORK:
+                    {
+                        socket_fsm = ACCEPT;
+                    }
+                    break;
+
+                    case SERVER_SOCKET_SUCCESS_CHILD:
+                    {
+                        socket_fsm = READ;
+                    }
+
+                    default:
+                    break;
+                }
+            }
+            break;
+
+            case REFUSE:
+            {
+                int refuse = SocketStateRefuse(client_socket);
+
+                if(refuse >= 0)
+                    socket_fsm = ACCEPT;
             }
             break;
 
             case READ:
             {
-                if(SocketStateRead(new_socket) <= 0)
+                if(SocketStateRead(client_socket) <= 0)
                 {
                     if(concurrent)
-                    {
                         socket_fsm = CLOSE;
-                    }
                     else
-                    {
                         socket_fsm = ACCEPT;
-                    }
                 }
             }
             break;
 
             case CLOSE:
             {
-                // By now, this state should not be reached.
-                if(SocketStateClose(new_socket) < 0)
+                if(SocketStateClose(client_socket) < 0)
                 {
                     socket_fsm = CLOSE;
                 }
--- Source_files/ServerSocketFSM.h
+++ Source_files/ServerSocketFSM.h
@@ -25,10 +25,19 @@
 #define SERVER_SOCKET_MSG_ACCEPT_OK         "Accept succeeded."
 #define SERVER_SOCKET_MSG_MAX_CONNS_REACHED "Cannot create a new server instance as maximum number of client connections have already been reached."
 #define SERVER_SOCKET_MSG_CANNOT_FORK       "Cannot fork the current server socket server process."
-#define SERVER_SOCKET_MSG_SERVER_INSTANCES  "Current number of server socket instances: <%d>."
 #define SERVER_SOCKET_MSG_NEW_PROCESS       "Created new server socket instance in process with PID <%d>."
+#define SERVER_SOCKET_MSG_REFUSE            "Connection refused by the server. Closing socket in %d seconds."
 #define SERVER_SOCKET_MSG_CLOSE_NOK         "An error happened while closing the socket."
 #define SERVER_SOCKET_MSG_CLOSE_OK          "Socket successfully closed."
+
+#define SERVER_SOCKET_ERR_CANNOT_FORK       -2
+#define SERVER_SOCKET_ERR_REFUSE_CONN       -1
+#define SERVER_SOCKET_SUCCESS_PARENT        0
+#define SERVER_SOCKET_SUCCESS_CHILD         1
+
+#define SERVER_SOCKET_LEN_MSG_REFUSE        100
+
+#define SERVER_SOCKET_SECONDS_AFTER_REFUSAL 0
 
 /************************************/
 
@@ -38,13 +47,15 @@
 
 typedef enum
 {
-    CREATE_FD = 0   ,
-    OPTIONS         ,
-    BIND            ,
-    LISTEN          ,
-    ACCEPT          ,
-    READ            ,
-    CLOSE           ,
+    CREATE_FD = 0       ,
+    OPTIONS             ,
+    BIND                ,
+    LISTEN              ,
+    ACCEPT              ,
+    MANAGE_CONCURRENCY  ,
+    REFUSE              ,
+    READ                ,
+    CLOSE               ,
 
 } SOCKET_FSM;
 
