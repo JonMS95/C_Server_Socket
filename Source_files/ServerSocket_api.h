2820199d083b512fa5d2ab6c7ebfe1830392e9ee
JonMS95 <jon_portugalete@hotmail.com>
2023-11-11 16:06:08 +0100

    Added Doxygen comment for ServerSocketRun function.

--- Source_files/ServerSocket_api.h
+++ Source_files/ServerSocket_api.h
@@ -18,10 +18,15 @@ extern "C" {
 /******** Function prototypes ********/
 /*************************************/
 
-/// @brief Socket Finite State Machine (FSM).
-/// @param server_port Port number which the socket is going to be listening to.
-/// @param max_conn_num Maximum amount of allowed connections.
-/// @return < 0 if it failed.
+/// @brief Runs server socket.
+/// @param server_port Port server is meant to be listening to.
+/// @param max_conn_num Maximum number of connections.
+/// @param concurrent Enable concurrent server instances.
+/// @param secure Enable secure communication (TLS).
+/// @param cert_path Path to server ceritificate.
+/// @param key_path Path to server private key.
+/// @param CustomSocketStateInteract Custom function to interact with client once connection is established.
+/// @return 0 always, exit sending failure signal if SIGINT signal handler could not be properly set.
 int ServerSocketRun(int server_port, int max_conn_num, bool concurrent, bool secure, char* cert_path, char* key_path, int (*CustomSocketStateInteract)(int client_socket, bool secure, SSL** ssl));
 
 /*************************************/
