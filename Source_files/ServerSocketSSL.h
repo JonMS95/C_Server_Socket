4157ed0024b182534772223d5d6f6f6bca9c5857
JonMS95 <jon_portugalete@hotmail.com>
2023-11-17 18:13:24 +0100

    Added ServerSocketSSLExpandPath function prototype.

--- Source_files/ServerSocketSSL.h
+++ Source_files/ServerSocketSSL.h
@@ -18,6 +18,7 @@
 
 #define SERVER_SOCKET_SSL_HANDSHAKE_SUCCESS 1
 
+#define SERVER_SOCKET_HOME_OS_DIR_NAME      "HOME"
 #define SERVER_SOCKET_MSG_PATH_TO_PRIV_KEY  "Path to certificate: <%s>"
 #define SERVER_SOCKET_MSG_PATH_TO_CERT      "Path to private key: <%s>"
 
@@ -29,6 +30,7 @@
 
 int ServerSocketSSLSetup(SSL_CTX** ctx, SSL** ssl, char* cert_path, char* priv_key_path);
 int ServerSocketSSLHandshake(int client_socket, SSL_CTX** ctx, SSL** ssl);
+void ServerSocketSSLExpandPath(char** dest_expanded_path, char** src_short_path);
 
 /*************************************/
 
