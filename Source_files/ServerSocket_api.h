9b983e68942d7c405b28e0235add2a68e4125442
JonMS95 <jon_portugalete@hotmail.com>
2024-04-27 20:18:54 +0200

    Added some new helpful function prototypes.

--- Source_files/ServerSocket_api.h
+++ Source_files/ServerSocket_api.h
@@ -12,6 +12,12 @@ extern "C" {
 #include <stdbool.h>
 #include <openssl/ssl.h>
 
+#ifdef __cplusplus
+#include <cstring>
+#else
+#include <string.h>
+#endif
+
 /*************************************/
 
 /*************************************/
@@ -26,16 +32,40 @@ extern "C" {
 /******** Function prototypes ********/
 /*************************************/
 
+/// @brief Get client's IP address.
+/// @param client_socket Client socket.
+/// @param client_IPv4 Target string to which client IPv4 address is meant to be copied.
+C_SERVER_SOCKET_API void ServerSocketGetClientIPv4(int client_socket, char* client_IPv4);
+
+/// @brief Reads from client socket.
+/// @param client_socket Client socket.
+/// @param rx_buffer Required RX buffer to read from socket.
+/// @return > 0 equaling the amount of bytes read, 0 if client got disconnected, < 0 if any error happened.
+C_SERVER_SOCKET_API int ServerSocketRead(int client_socket, char* rx_buffer, unsigned long rx_buffer_size);
+
+#define SERVER_SOCKET_READ(client_socket, rx_buffer)                \
+        ServerSocketRead(client_socket, rx_buffer, sizeof(rx_buffer))
+
+/// @brief Writes to client socket.
+/// @param client_socket Client socket.
+/// @param tx_buffer Required TX buffer in which data to write is found.
+/// @return > 0 equaling the amount of bytes written, 0 if client got disconnected, < 0 if any error happened.
+C_SERVER_SOCKET_API int ServerSocketWrite(int client_socket, char* tx_buffer, unsigned long tx_buffer_size);
+
+#define SERVER_SOCKET_WRITE(client_socket, tx_buffer)                \
+        ServerSocketWrite(client_socket, tx_buffer, strlen(tx_buffer))
+
 /// @brief Runs server socket.
 /// @param server_port Port server is meant to be listening to.
 /// @param max_conn_num Maximum number of connections.
 /// @param concurrent Enable concurrent server instances.
+/// @param non_blocking Tells whether or not is the socket meant to be non-blocking
 /// @param secure Enable secure communication (TLS).
 /// @param cert_path Path to server ceritificate.
 /// @param key_path Path to server private key.
 /// @param CustomSocketStateInteract Custom function to interact with client once connection is established.
 /// @return 0 always, exit sending failure signal if SIGINT signal handler could not be properly set.
-C_SERVER_SOCKET_API int ServerSocketRun(int server_port, int max_conn_num, bool concurrent, bool secure, char* cert_path, char* pkey_path, int (*CustomSocketStateInteract)(int client_socket, bool secure, SSL** ssl));
+C_SERVER_SOCKET_API int ServerSocketRun(int server_port, int max_conn_num, bool concurrent, bool non_blocking, bool secure, char* cert_path, char* pkey_path, int (*CustomSocketStateInteract)(int client_socket));
 
 /*************************************/
 
