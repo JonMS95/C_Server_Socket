d8c2a5441646aea36a11ebb560e30c4d0052c703
JonMS95 <jon_portugalete@hotmail.com>
2024-05-18 12:53:13 +0200

    Added reuse port, reuse address and rx timeout input parameters to SocketOptions, added new definition for 1 second in microseconds.

--- Source_files/ServerSocketUse.h
+++ Source_files/ServerSocketUse.h
@@ -20,6 +20,8 @@
 #define SERVER_SOCKET_MSG_ERR_GET_SOCKET_FLAGS  "Error while getting socket flags."
 #define SERVER_SOCKET_MSG_ERR_SET_SOCKET_FLAGS  "Error while setting O_NONBLOCK flag."
 
+#define SERVER_SOCKET_1_SEC_IN_USECS            1000000
+
 /***********************************/
 
 /*************************************/
@@ -27,7 +29,7 @@
 /*************************************/
 
 int CreateSocketDescriptor(int domain, int type, int protocol);
-int SocketOptions(int socket_desc, int reuse_address, int reuse_port, int keep_idle, int keep_counter, int keep_interval, int keep_alive);
+int SocketOptions(int socket_desc, bool reuse_address, bool reuse_port, unsigned long rx_timeout_usecs);
 struct sockaddr_in PrepareForBinding(sa_family_t address_family, in_addr_t allowed_IPs, uint16_t listen_port);
 int BindSocket(int socket_desc, struct sockaddr_in server);
 int SocketListen(int socket_desc, int connections_number);
