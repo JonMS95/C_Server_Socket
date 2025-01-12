018988778a770ce75320e18d3b88ca99594b604d
JonMS95 <jon_portugalete@hotmail.com>
2025-01-12 17:36:00 +0100

    Added some definitions.

--- Source_files/ServerSocketManageThreads.h
+++ Source_files/ServerSocketManageThreads.h
@@ -17,7 +17,18 @@
 #define SERVER_SOCKET_MANAGE_THREADS_SUCCESS            0
 #define SERVER_SOCKET_THREADS_ALLOCATION_FAILURE        -1
 #define SERVER_SOCKET_MANAGE_THREAD_CREATION_FAILURE    -2
-#define SERVER_SOCKET_DATA_CLEAN_FAILURE                -3
+#define SERVER_SOCKET_MANAGE_THREAD_NO_FREE_SPOTS       -3
+#define SERVER_SOCKET_DATA_CLEAN_FAILURE                -4
+
+#define SERVER_SOCKET_MSG_ERR_CREATE_THREAD         "An error ocurred while creating thread: <%s>"
+#define SERVER_SOCKET_MSG_ERR_NO_FREE_SPOTS         "Maximum number of connections has already been reached: <%d>"
+#define SERVER_SOCKET_MSG_THREAD_CREATION_SUCCESS   "Successfully created thread for client socket <%d> (TID: <%lu>)"
+
+#define SERVER_SOCKET_MSG_SSL_HANDSHAKE_NOK     "SSL handshake failed."
+#define SERVER_SOCKET_MSG_SSL_HANDSHAKE_OK      "SSL handshake succeeded."
+
+#define SERVER_SOCKET_MSG_CLOSE_NOK             "An error happened while closing socket <%d>."
+#define SERVER_SOCKET_MSG_CLOSE_OK              "Socket <%d> successfully closed."
 
 /************************************/
 
@@ -26,8 +37,8 @@
 /************************************/
 
 int SocketSetupThreads(int max_conn_num, bool secure, bool non_blocking, int (*interact_fn)(int client_socket));
-int SocketStateManageThreads(int client_socket);
-int SocketKillAllThreads();
+int SocketLaunchServerInstance(int client_socket);
+int SocketFreeThreadsResources();
 
 /************************************/
 
