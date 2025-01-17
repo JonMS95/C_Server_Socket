9eaa030b4740189761a6f4f94a681ad036f4af60
JonMS95 <jon_portugalete@hotmail.com>
2025-01-17 18:58:09 +0100

    Since header file does no longer exist, all of its content has now been moved to the current source file.

--- Source_files/ServerSocketHelperFunctions.c
+++ Source_files/ServerSocketHelperFunctions.c
@@ -1,9 +1,30 @@
+/************************************/
+/******** Include statements ********/
+/************************************/
+
 #include <arpa/inet.h>      // sockaddr_in, inet_addr
 #include <unistd.h>
-#include "ServerSocketHelperFunctions.h"
 #include "ServerSocketSSL.h"
 #include "SeverityLog_api.h"
 #include "ServerSocket_api.h"
+
+/************************************/
+
+/***********************************/
+/******** Define statements ********/
+/***********************************/
+
+#define getName(var)  #var
+
+#define SERVER_SOCKET_HELPER_MSG_INSUFFICIENT_RX_BUFFER_SIZE    "Could not read from buffer due to insufficient RX buffer size."
+#define SERVER_SOCKET_HELPER_MSG_RX_BUFFER_NAME                 "RX buffer variable name: <%s>, declared size: <%d>."
+#define SERVER_SOCKET_HELPER_MSG_INSUFFICIENT_TX_BUFFER_SIZE    "Could not write to buffer due to insufficient TX buffer size."
+#define SERVER_SOCKET_HELPER_MSG_TX_BUFFER_NAME                 "TX buffer variable name: <%s>, declared size: <%d>."
+
+#define SERVER_SOCKET_HELPER_ERR_INSUFFICIENT_RX_BUFFER_SIZE    -1
+#define SERVER_SOCKET_HELPER_ERR_INSUFFICIENT_TX_BUFFER_SIZE    -2
+
+/***********************************/
 
 /// @brief Get client's IP address.
 /// @param client_socket Client socket.
