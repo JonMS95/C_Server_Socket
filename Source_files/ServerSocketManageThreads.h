be1a5dd92b1215e8c7a9cb25e195016f87416fcc
JonMS95 <jon_portugalete@hotmail.com>
2025-01-17 19:00:19 +0100

    Removed define statements as well as most of the includes. Only the necessary ones (i.e., the ones required by the shared function prototypes will remain).

--- Source_files/ServerSocketManageThreads.h
+++ Source_files/ServerSocketManageThreads.h
@@ -5,30 +5,7 @@
 /******** Include statements ********/
 /************************************/
 
-#include <pthread.h>
 #include <stdbool.h>
-
-/************************************/
-
-/************************************/
-/********* Define statements ********/
-/************************************/
-
-#define SERVER_SOCKET_MANAGE_THREADS_SUCCESS            0
-#define SERVER_SOCKET_THREADS_ALLOCATION_FAILURE        -1
-#define SERVER_SOCKET_MANAGE_THREAD_CREATION_FAILURE    -2
-#define SERVER_SOCKET_MANAGE_THREAD_NO_FREE_SPOTS       -3
-#define SERVER_SOCKET_DATA_CLEAN_FAILURE                -4
-
-#define SERVER_SOCKET_MSG_ERR_CREATE_THREAD         "An error ocurred while creating thread: <%s>"
-#define SERVER_SOCKET_MSG_ERR_NO_FREE_SPOTS         "Maximum number of connections has already been reached: <%d>"
-#define SERVER_SOCKET_MSG_THREAD_CREATION_SUCCESS   "Successfully created thread for client socket <%d> (TID: <%lu>)"
-
-#define SERVER_SOCKET_MSG_SSL_HANDSHAKE_NOK     "SSL handshake failed."
-#define SERVER_SOCKET_MSG_SSL_HANDSHAKE_OK      "SSL handshake succeeded."
-
-#define SERVER_SOCKET_MSG_CLOSE_NOK             "An error happened while closing socket <%d>."
-#define SERVER_SOCKET_MSG_CLOSE_OK              "Socket <%d> successfully closed."
 
 /************************************/
 
