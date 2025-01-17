0ceca995d93c505861ae3b53f8fd02dbec5d7319
JonMS95 <jon_portugalete@hotmail.com>
2025-01-17 18:57:38 +0100

    Static function prototypes, private typedefs and includes do now exist only within source file.

--- Source_files/ServerSocketFSM.c
+++ Source_files/ServerSocketFSM.c
@@ -6,17 +6,66 @@
 #include <unistd.h>         // Fork if concurrency is accepted.
 #include <string.h>         // strlen
 #include <signal.h>         // Shutdown signal.
-#include <openssl/ssl.h>
 #include <openssl/err.h>
 #include "ServerSocketUse.h"
-#include "ServerSocketFSM.h"
-// #include "ServerSocketConcurrency.h"
 #include "ServerSocketManageThreads.h"
 #include "ServerSocketSSL.h"
 #include "ServerSocketDefaultInteract.h"
+#include "ServerSocket_api.h"
 #include "SeverityLog_api.h"
 
 /************************************/
+
+/***********************************/
+/******** Define statements ********/
+/***********************************/
+
+#define SERVER_SOCKET_SET_SIGINT_ERR            "Error while trying to set up SIGINT handler."
+#define SERVER_SOCKET_MSG_SIGINT_RECEIVED       "Received Ctrl+C (SIGINT). Cleaning up and exiting."
+#define SERVER_SOCKET_MSG_THREADS_CLEAN         "Cleaning up server instances."
+#define SERVER_SOCKET_MSG_CLEANING_UP_SSL_CTX   "Cleaning up server SSL context."
+#define SERVER_SOCKET_MSG_CLEANING_UP_SSL       "Cleaning up server SSL data."
+#define SERVER_SOCKET_MSG_CREATION_NOK          "Socket file descriptor creation failed."
+#define SERVER_SOCKET_MSG_CREATION_OK           "Socket file descriptor created."
+#define SERVER_SOCKET_MSG_SETUP_SSL_NOK         "SSL setup failed."
+#define SERVER_SOCKET_MSG_SETUP_SSL_OK          "SSL setup succeeded."
+#define SERVER_SOCKET_MSG_SET_OPTIONS_NOK       "Failed to set socket options."
+#define SERVER_SOCKET_MSG_SET_OPTIONS_OK        "Successfully set socket options."
+#define SERVER_SOCKET_MSG_BIND_NOK              "Socket binding failed."
+#define SERVER_SOCKET_MSG_BIND_OK               "Socket file descriptor binded."
+#define SERVER_SOCKET_MSG_LISTEN_NOK            "Socket listen failed."
+#define SERVER_SOCKET_MSG_LISTEN_OK             "Socket listen succeeded."
+#define SERVER_SOCKET_MSG_ACCEPT_NOK            "Accept failed."
+#define SERVER_SOCKET_MSG_ACCEPT_OK             "Accept succeeded."
+#define SERVER_SOCKET_MANAGE_THREADS_NOK        "Server instance creation failed."
+#define SERVER_SOCKET_MANAGE_THREADS_OK         "Server instance creation succeeded."
+#define SERVER_SOCKET_MSG_REFUSE                "Connection refused by the server. Closing socket in %d seconds."
+
+#define SERVER_SOCKET_LEN_MSG_REFUSE            100
+
+#define SERVER_SOCKET_SECONDS_AFTER_REFUSAL     0
+
+/************************************/
+
+/**********************************/
+/******** Type definitions ********/
+/**********************************/
+
+typedef enum
+{
+    CREATE_FD = 0       ,
+    SETUP_SSL           ,
+    OPTIONS             ,
+    BIND                ,
+    LISTEN              ,
+    ACCEPT              ,
+    MANAGE_THREADS      ,
+    REFUSE              ,
+    CLOSE               ,
+
+} SOCKET_FSM;
+
+/**********************************/
 
 /***********************************/
 /******** Private variables ********/
@@ -25,6 +74,29 @@
 static volatile int ctrlCPressed        = 0;
 
 /***********************************/
+
+/*************************************/
+/**** Private function prototypes ****/
+/*************************************/
+
+static void SocketFreeResources(void);
+static void SocketSIGINTHandler(int signum);
+static int SocketStateCreate(void);
+static int SocketStateOptions(  int             socket_desc     ,
+                                bool            reuse_address   ,
+                                bool            reuse_port      ,
+                                unsigned long   rx_timeout_secs ,
+                                unsigned long   rx_timeout_usecs,
+                                unsigned long   tx_timeout_secs ,
+                                unsigned long   tx_timeout_usecs);
+static int SocketStateSetupSSL(const char* cert_path, const char* priv_key_path);
+static int SocketStateBind(int socket_desc, int server_port);
+static int SocketStateListen(int socket_desc, int max_conn_num);
+static int SocketStateAccept(int socket_desc, bool non_blocking);
+static int SocketStateManageThreads(int client_socket);
+static int SocketStateRefuse(int client_socket);
+
+/*************************************/
 
 /*************************************/
 /******* Function definitions ********/
