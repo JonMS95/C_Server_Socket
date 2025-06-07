1c1f00ea2a07ee30fc77819d0713742fd5ffa6cb
JonMS95 <jon_portugalete@hotmail.com>
2025-06-07 02:31:24 +0200

    Modified certificate/private key generating script.

--- sh/create_self_signed_cert.sh
+++ sh/create_self_signed_cert.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+CERT_DIR_PATH="$(dirname $(realpath ${0}))/../certificate_test"
+PKEY_PATH="${CERT_DIR_PATH}/private.key"
+CERT_PATH="${CERT_DIR_PATH}/certificate.crt"
+
+# Create directory if it doesn't exist
+if [ ! -d ${CERT_DIR_PATH} ]
+then
+    mkdir ${CERT_DIR_PATH}
+fi
+
 # Generate private key first
-openssl genpkey -algorithm RSA -out private.key
+openssl genpkey -algorithm RSA -out ${PKEY_PATH}
 
 # Update permissions just in case
-chmod 600 private.key
+chmod 600 ${PKEY_PATH}
 
 # Generate self-signed certificate
-openssl req -new -key private.key -x509 -days 365 -out certificate.crt
+openssl req -new -key ${PKEY_PATH} -x509 -days 365 -out ${CERT_PATH}
 
 # View certificate
-openssl x509 -in certificate.crt -text
+openssl x509 -in ${CERT_PATH} -text
